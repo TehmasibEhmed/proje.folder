9c673caba1be2ef3697e0f1f90a47d1dd53188c2
tahmasibahmad <123749451+TehmasibEhmed@users.noreply.github.com>
2023-09-02 13:09:00 +0400

    yadda saxla

---
+++ assets/data/data.js
@@ -0,0 +1,95 @@
+export const data=[
+    {
+      "id": 1,
+      "title": "The Great Gatsby",
+      "author": "F. Scott Fitzgerald",
+      "isbn": "9780743273565",
+      "image": "https://example.com/images/book1.jpg"
+    },
+    {
+      "id": 2,
+      "title": "To Kill a Mockingbird",
+      "author": "Harper Lee",
+      "isbn": "9780061120084",
+      "image": "https://example.com/images/book2.jpg"
+    },
+    {
+      "id": 3,
+      "title": "1984",
+      "author": "George Orwell",
+      "isbn": "9780451524935",
+      "image": "https://example.com/images/book3.jpg"
+    },
+    {
+      "id": 4,
+      "title": "Pride and Prejudice",
+      "author": "Jane Austen",
+      "isbn": "9780141439518",
+      "image": "https://example.com/images/book4.jpg"
+    },
+    {
+      "id": 5,
+      "title": "The Catcher in the Rye",
+      "author": "J.D. Salinger",
+      "isbn": "9780316769174",
+      "image": "https://example.com/images/book5.jpg"
+    },
+    {
+      "id": 6,
+      "title": "The Hobbit",
+      "author": "J.R.R. Tolkien",
+      "isbn": "9780547928227",
+      "image": "https://example.com/images/book6.jpg"
+    },
+    {
+      "id": 7,
+      "title": "Moby-Dick",
+      "author": "Herman Melville",
+      "isbn": "9780142437247",
+      "image": "https://example.com/images/book7.jpg"
+    },
+    {
+      "id": 8,
+      "title": "The Lord of the Rings",
+      "author": "J.R.R. Tolkien",
+      "isbn": "9780544003415",
+      "image": "https://example.com/images/book8.jpg"
+    },
+    {
+      "id": 9,
+      "title": "Jane Eyre",
+      "author": "Charlotte Bronte",
+      "isbn": "9780141441146",
+      "image": "https://example.com/images/book9.jpg"
+    },
+    {
+      "id": 10,
+      "title": "The Alchemist",
+      "author": "Paulo Coelho",
+      "isbn": "9780062315007",
+      "image": "https://example.com/images/book10.jpg"
+    },
+    {
+      "id": 11,
+      "title": "The Odyssey",
+      "author": "Homer",
+      "isbn": "9780140268867",
+      "image": "https://example.com/images/book11.jpg"
+    },
+    {
+      "id": 12,
+      "title": "Harry Potter and the Sorcerer's Stone",
+      "author": "J.K. Rowling",
+      "isbn": "9780590353427",
+      "image": "https://example.com/images/book12.jpg"
+    },
+    {
+      "id": 13,
+      "title": "Crime and Punishment",
+      "author": "Fyodor Dostoevsky",
+      "isbn": "9780143107637",
+      "image": "https://example.com/images/book13.jpg"
+    }
+]
+  
+
